b88a6fe0b77081695951aeeabae828045f9a9d73
Markel-Fernandez-de-Labastida <markelfdz8@gmail.com>
2025-05-09 10:04:33 +0200

    Tercer ejercicio completado

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ Declara una función que acepte una palabra como parámetro y la devuelva con to
     2. convertimos el array de caracteres a string con un espacio en blanco entre cada caracter con join()
     3. Pasamos a mayusculas todos los caracteres con toLocaleUpperCase()*/
 
-/*  He elegido este nombre porque dice explicitamente lo que hace la función*/
+/*  He elegido este nombre porque separa los caracteres de la palabra y los pone en mayusculas*/
 const separarCaracteresYAMayusculas = (palabra) => {
     return palabra.split("").join(" ").toLocaleUpperCase();
 }
@@ -24,3 +24,24 @@ const numeroPalabras = (nombre) => {
 }
 
 console.log(numeroPalabras("Antonio Alberto Jesús"));
+
+/* Ejercicio 3: Diseña un algoritmo que cuente las veces que aparece una determinada letra en una frase. */
+
+/*  1. Leer tanto la frase como la letra a buscar
+    2. En un bucle donde recorre toda la frase compruebas si es la letra introducida por el usuario y sumas uno si concuerdan. Nada si son diferentes
+    3. Muestra la cantidad de veces que aparece la letra en pantalla*/
+
+/*
+Inicio
+    Leer frase
+    Leer letra
+    cont = 0;
+    i = 0
+    Mientras i <= frase.length-1 Entonces
+        si i == letra Entonces
+            cont suma 1
+        Fin si
+    Escribir "la letra ${letra} aparece ${cont} veces]"
+Fin
+*/
+
